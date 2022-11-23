5bf2727086ea0512ea90f232fcd1b0b80b496607
Christian Chiama <beengineering@icloud.com>
2022-11-23 12:02:27 +0100

    add some project under api

---
+++ api/treatment-api/src/main/java/it/wefox/quarkus/Main.java
@@ -0,0 +1,8 @@
+package it.wefox.quarkus;/**
+ * @author Christian Chiama
+ *         --
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
